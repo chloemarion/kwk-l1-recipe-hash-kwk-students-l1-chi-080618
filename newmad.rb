54244a8063e258f0ac386a106480fa5332382b55
chloemarion <lacymendes98@gmail.com>
2018-08-09 18:57:52 +0000

    automatically backed up by learn

---
+++ newmad.rb
@@ -0,0 +1,25 @@
+def option_a 
+  puts "give me a noun"
+  noun1 = gets.chomp 
+  puts "give me a color"
+  color1 = gets.chomp 
+  puts "give me a name with an 's'"
+  name1 = gets.chomp 
+  puts "give me a place"
+  place1 = gets.chomp 
+  puts "give me a verb ending in 'ing'"
+  verbing1 = gets.chomp 
+  puts "give me another name"
+  name2 = gets.chomp 
+  puts "give me another name"
+  name3 = gets.chomp 
+  puts "give me an adjective"
+  adjective1 = gets.chomp 
+  puts "FINALLY give me another noun"
+  noun2 = gets.chomp 
+  
+  puts "So I needed to go shopping to buy a #{noun1}. I knew #{color1} wrapping paper was #{name1} favorite so I definitely needed to buy it at #{place1}. As I was #{verbing1} to the store with my friend #{name2}, #{name3} called me and reminded me to purchase a #{adjective1} #{noun2}."
+end
+
+option_a
+  
